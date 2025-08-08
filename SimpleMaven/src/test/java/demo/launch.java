768bbc295152d1b972a743fd75eba55b99165294
MUKIL <MUKIL@DESKTOP-I23RP3R>
2025-08-08 11:36:37 +0530

    Changes in TC_09.java

--- SimpleMaven/src/test/java/demo/launch.java
+++ SimpleMaven/src/test/java/demo/launch.java
@@ -1,7 +1,13 @@
 package demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class launch {
 	public static void main(String[] args) {
 		System.out.println("fork and pull request");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
 	}
 }
--- SimpleMaven/src/test/java/pallavi/TC_09.java
+++ SimpleMaven/src/test/java/pallavi/TC_09.java
@@ -1,7 +1,18 @@
 package pallavi;
 
+import org.testng.Assert;
+
+import com.aventstack.extentreports.Status;
+
+import objectrepository.HomePage;
+
 public class TC_09 {
- public static void main(String[] args) {
-	System.out.println("hello");
+	
+	
+	public void clickOnBooks() {
+		HomePage hp=new HomePage(driver);
+		hp.getBooks().click();
+		Assert.assertEquals(driver.getTitle(),"Demo Web Shop. Books", "Books page is not displayed");
+		test.log(Status.PASS, "Books page is displayed");
 }
 }
